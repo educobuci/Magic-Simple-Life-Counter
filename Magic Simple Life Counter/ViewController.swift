2a15f152eb6adb27e735e3ade5c09b63d9b86efe
Eduardo Cobuci <educobuci@gmail.com>
2016-07-09 23:41:00 -0300

    Implement life count

--- Magic Simple Life Counter/ViewController.swift
+++ Magic Simple Life Counter/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Outlets
     @IBOutlet weak var opponentLifeLabel: UILabel!
     @IBOutlet weak var playerLifeLabel: UILabel!
+    // Locals
+    var playerLife = 20
+    var opponentLife = 20
     override func viewDidLoad() {
         super.viewDidLoad()
         self.opponentLifeLabel.transform = CGAffineTransformMakeScale(-1, -1);
+    }
+    
+    @IBAction func decreaseOpponentLife() {
+        opponentLife -= 1
+        opponentLifeLabel.text = String(opponentLife)
+    }
+    
+    @IBAction func increaseOpponentLife() {
+        opponentLife += 1
+        opponentLifeLabel.text = String(opponentLife)
+    }
+    
+    @IBAction func increasePlayerLife() {
+        playerLife += 1
+        playerLifeLabel.text = String(playerLife)
+    }
+    
+    @IBAction func decreasePlayerLife() {
+        playerLife -= 1
+        playerLifeLabel.text = String(playerLife)
     }
 }
 
